e1ba1c722ff83db8fe6d7da7a013cdec7f905fb4
Muhammad Adeel Nawaz <adeel.0202@gmail.com>
2016-03-13 14:07:14 +0500

    Birthday Quiz taken on 11th march

---
+++ Quiz Birthday/DaysToBirthday.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_Birthday
+{
+    class DaysToBirthday
+    {
+
+        static void Main(string[] args)
+        {
+            DaysToBirthday obj = new DaysToBirthday();
+            obj.getBirthday();
+            Console.ReadLine();
+        }
+
+        private void getBirthday()
+        {
+            DateTime dob;
+            Console.Write("Enter your birthday: ");
+            DateTime.TryParse(Console.ReadLine(), out dob);
+            calculateDays(dob);
+        }
+
+        private void calculateDays(DateTime dob)
+        {
+            TimeSpan age = DateTime.Today.Subtract(dob);
+            int years = (int)(age.Days / 365.25);
+            int balance = (int)(age.Days - (years * 365.25));
+            int daysLeft = 365 - balance;
+            Console.WriteLine("Days left to your birthday: " + daysLeft);
+        }
+    }
+}

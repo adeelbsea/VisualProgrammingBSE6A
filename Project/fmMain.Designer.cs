11ca2319e828a892567044688dc128b67bef3186
Muhammad Adeel Nawaz <adeel.0202@gmail.com>
2016-04-10 12:22:15 +0500

    Forms updated

---
+++ Project/fmMain.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Project
+{
+    partial class fmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fmMain));
+            this.lblKeyboard = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblKeyboard
+            // 
+            this.lblKeyboard.AutoSize = true;
+            this.lblKeyboard.BackColor = System.Drawing.Color.LightGreen;
+            this.lblKeyboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKeyboard.Location = new System.Drawing.Point(138, 100);
+            this.lblKeyboard.Name = "lblKeyboard";
+            this.lblKeyboard.Size = new System.Drawing.Size(295, 55);
+            this.lblKeyboard.TabIndex = 0;
+            this.lblKeyboard.Text = "Start Testing";
+            this.lblKeyboard.Click += new System.EventHandler(this.lblKeyboard_Click);
+            // 
+            // fmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(558, 395);
+            this.Controls.Add(this.lblKeyboard);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fmMain";
+            this.Text = "Typing Speed Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKeyboard;
+    }
+}

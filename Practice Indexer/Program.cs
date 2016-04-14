0b227b1c03e208faf2f1bc3183427beeef10ba9c
Muhammad Adeel Nawaz <adeel.0202@gmail.com>
2016-04-14 22:46:27 +0500

    Project was deleted. Committing again.

---
+++ Practice Indexer/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Indexer
+{
+    class Program
+    {
+        float x, y, z;
+
+        public void myValue(float d)
+        {
+            x *= d;
+            y *= d;
+            z *= d;
+        }
+
+        public float this[string i]
+        {
+            set
+            {
+                switch(i)
+                {
+                    case "X":
+                        x = value;
+                        break;
+                    case "Y":
+                        y = value;
+                        break;
+                    case "Z":
+                        z = value;
+                        break;
+                }
+            }
+
+            get
+            {
+                switch(i)
+                {
+                    case "X":
+                        return x;
+                    case "Y":
+                        return y;
+                    case "Z":
+                        return z;
+                }
+                return 0;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program ob = new Program();
+            ob["X"] = 10.0f;
+            ob["Y"] = 20.0f;
+            ob["Z"] = 30.0f;
+
+            ob.myValue(2);
+            Console.WriteLine(ob["Y"]);
+
+            Console.ReadLine();
+        }
+    }
+}
